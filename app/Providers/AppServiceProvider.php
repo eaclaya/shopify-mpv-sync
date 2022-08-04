8484cd21cb487fddfc9aa2737f68647e331b47d6
MottAndBow <mb@MottAndBows-MacBook-Pro.local>
2022-08-03 20:50:04 -0600

    Add service provider config for shopify context

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\ShopifyService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton('Shopify', function () {
+            return new ShopifyService(
+                $this->app->make(ShopifyService::class)
+            );
+        });
     }
 
     /**
@@ -23,6 +28,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
     }
 }
