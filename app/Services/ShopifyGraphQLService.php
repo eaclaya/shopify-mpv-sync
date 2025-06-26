356b8a9c84eef217415308276a2589392bc0e145
lajy-enterprise <50001481+lajy-enterprise@users.noreply.github.com>
2025-06-26 02:11:08 -0400

    Update ShopifyGraphQLService.php

--- app/Services/ShopifyGraphQLService.php
+++ app/Services/ShopifyGraphQLService.php
@@ -139,7 +139,7 @@ class ShopifyGraphQLService
                     [
                         'inventoryItemId' => $inventoryItemId,
                         'locationId' => $locationGlobalId,
-                        'availableQuantity' => $newQuantity
+                        'quantity' => $newQuantity
                     ]
                 ]
             ]
