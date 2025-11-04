50ee366cbfba4b5eac696373db14e22cd4759749
lajy-enterprise <50001481+lajy-enterprise@users.noreply.github.com>
2025-11-03 23:59:19 -0400

    Update GetProductsByPaginate.php

--- app/Console/Commands/GetProductsByPaginate.php
+++ app/Console/Commands/GetProductsByPaginate.php
@@ -33,7 +33,7 @@ class GetProductsByPaginate extends Command
         // Acá va la lógica que quieras ejecutar
         $this->info('Running GetProductsByPaginate command...');
 
-        $products = ShopifyGraphQL::getProductsByPaginate(50, null);
+        $products = ShopifyGraphQL::getProductsByPaginate(50, 'eyJsYXN0X2lkIjo3NzYwMTQyNjMxMTQwLCJsYXN0X3ZhbHVlIjoiNzc2MDE0MjYzMTE0MCJ9');
         // $edgesPublications = $publications['data']['publications']['edges'];
         // $arrayPublications = [];
         // foreach ($edgesPublications as $edge) {
