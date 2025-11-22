368a81bff7bffccd67e599ca697185154ecadeff
lajy-enterprise <50001481+lajy-enterprise@users.noreply.github.com>
2025-11-22 14:43:20 -0400

    Update CheckBearerToken.php

--- app/Http/Middleware/CheckBearerToken.php
+++ app/Http/Middleware/CheckBearerToken.php
@@ -20,17 +20,17 @@ class CheckBearerToken
         Log::info('entro en el middleware');
         $authHeader = $request->header('Authorization');
         Log::info($authHeader);
-        if (!$authHeader || !str_starts_with($authHeader, 'Bearer ')) {
-            Log::info('Unauthorized');
+        if (!$authHeader || !str_starts_with($authHeader, 'Bearer')) {
+            Log::info('Unauthorized por no tener authHeader');
             return response()->json(['message' => 'Unauthorized'], 401);
         }
 
         $token = substr($authHeader, 7);
-
         $internalToken = '3|'.config('app.internal_bearer_token', '42ccd0251d486204d262cc4b2f6412a53268f238d99d871e91f65457151d89c4');
 
         if ($token !== $internalToken) {
-            Log::info('Unauthorized');
+            Log::info('Unauthorized por no coincidir authHeader');
+            Log::info("Token: $token , internalToken: $internalToken");
             return response()->json(['message' => 'Unauthorized'], 401);
         }
 
